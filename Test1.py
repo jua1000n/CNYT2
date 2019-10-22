5758f069411e87272afe3f5fc3b8984df2ff5241
jua1000n <53835422+jua1000n@users.noreply.github.com>
2019-10-22 13:24:57 -0500

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -17,7 +17,7 @@ class MyTestCase(unittest.TestCase):
            [3],
            [10]]
         d=1
-        c=[[0], [0], [9], [5], [12], [1]]
+        c=[[0], [0], [12], [5], [1], [9]]
         self.assertEqual(lib1.canic(a,b,d), c)
     #2. Experimentos de las múltiples rendijas clásico probabilístico, con más de dos rendijas.
     def test_canic(self):
@@ -40,6 +40,14 @@ class MyTestCase(unittest.TestCase):
         d=2
         c=[[0], [0], [0], [1/6], [1/6], [1/3], [1/6], [1/6],]
         self.assertEqual(lib1.canic(a,b,d), c)
+    #3 Experimento de las múltiples rendijas cuántico.
+    def test_cuan(self):
+        a = [[(0,0),(0,0),(0,0),(0,0),(0,0),(0,0)],[(0,0),(0,0),(0,0),(0,0),(0,0),(0,0)],[(0,0),(1,0),(0,0),(0,0),(0,0),(1,0)],[(0,0),(0,0),(0,0),(1,0),(0,0),(0,0)],[(0,0),(0,0),(1,0),(0,0),(0,0),(0,0)],[(1,0),(0,0),(0,0),(0,0),(1,0),(0,0)]]
+        b = [[(6,0)],[(2,0)],[(1,0)],[(5,0)],[(3,0)],[(10,0)]]
+        c = 1
+        esperado = [[(0, 0)], [(0, 0)], [(12, 0)], [(5, 0)], [(1, 0)], [(9, 0)]]
+        self.assertEqual(lib1.canic(a,b,c), esperado)
+
 
 if __name__ == '__main__':
     unittest.main()
